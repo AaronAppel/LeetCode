a98f99524bde9f9301d63cd852cbbeb9de05595f
Aaron Appel <36603239+AaronAppel@users.noreply.github.com>
2025-10-25 21:54:58 -0400

    [ProblemTemplates] Adding more problem template files
    Worked on C++ quizzes today so not much to commit

---
+++ Source/26.RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,36 @@
+
+#include <assert.h>
+#include <vector>
+
+// https: Link to leetcode problem
+
+int RemoveDuplicatesFromSortedArray_v1(std::vector<int>& nums)
+{
+	return 0;
+}
+
+#include "Testing.h"
+
+typedef int(*SolutionFuncPtr)(std::vector<int>&);
+
+void main_RemoveDuplicatesFromSortedArray()
+{
+	SolutionFuncPtr solutionFunc = RemoveDuplicatesFromSortedArray_v1;
+
+	// LeetCode cases
+	std::vector<int> input = {};
+	assert(solutionFunc(input) == 0);
+
+	// My cases
+	// assert(solutionFunc() == true);
+
+	// Test solution performance
+	std::vector<SolutionFuncPtr> funcs = {
+		RemoveDuplicatesFromSortedArray_v1
+	};
+
+	// RunSolutions(funcs, output, input);
+	// int bp = 0;
+}
+
+// int main() { main_RemoveDuplicatesFromSortedArray(); }
